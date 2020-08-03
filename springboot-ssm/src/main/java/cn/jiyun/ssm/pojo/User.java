6b6c8a29fa51f55e33fa3ca8d79702722dd91901
MaoJJ <2815796681@qq.com>
2020-08-03 15:52:47 +0800

    Day02

---
+++ springboot-ssm/src/main/java/cn/jiyun/ssm/pojo/User.java
@@ -0,0 +1,76 @@
+package cn.jiyun.ssm.pojo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//自增长
+    private Long id;
+    private String uname;
+    private String password;
+    private String name;
+    private Integer age;
+    private Integer sex;
+    private Date birthday;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ springboot-ssm/src/main/java/cn/jiyun/ssm/service/UserService.java
@@ -0,0 +1,24 @@
+package cn.jiyun.ssm.service;
+
+import cn.jiyun.ssm.mapper.UserMapper;
+import cn.jiyun.ssm.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryUserById(Long id){
+        return this.userMapper.selectByPrimaryKey(id);
+    }
+
+    public void deleteUserById(Long id){
+        this.userMapper.deleteByPrimaryKey(id);
+    }
+
+
+
+
+}
